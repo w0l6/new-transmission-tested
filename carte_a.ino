3e943a61313c0cc392e670ae9bb8d338698e836b
Wolf <141444067+w0l6@users.noreply.github.com>
2025-04-16 10:42:51 +0200

    Add files via upload

---
+++ carte_a.ino
@@ -0,0 +1,40 @@
+int pinData = 4;  // (clock)
+int pinSend = 3;  // envoyer l'octet
+
+void setup() {
+  pinMode(pinData, OUTPUT); 
+  pinMode(pinSend, OUTPUT); 
+  Serial.begin(9600); 
+}
+
+void loop() {
+  byte dataToSend = 128; 
+  envoyer(dataToSend);    
+  delay(1000);            
+}
+
+
+void envoyer(byte dataToSend) {
+  Serial.print("Envoi de l'octet: ");
+  Serial.println(dataToSend, DEC); 
+
+ 
+  digitalWrite(pinData, HIGH); 
+  delay(50);  
+  digitalWrite(pinData, LOW);   
+
+  
+  for (int i = 7; i >= 0; i--) {
+    bool bit = (dataToSend >> i) & 1;  
+    Serial.print("Envoi du bit: ");
+    Serial.println(bit); 
+
+    if (bit) {
+      digitalWrite(pinSend, HIGH);  
+    } else {
+      digitalWrite(pinSend, LOW);  
+    }
+    delay(100);  
+  }
+}
+
---
+++ carte_b.ino
@@ -0,0 +1,41 @@
+int pinData = 4;  // clock
+int pinReceive = 3;  // recevoir l'octet
+
+byte receivedData = 0; 
+
+void setup() {
+  pinMode(pinData, INPUT);  
+  pinMode(pinReceive, INPUT); 
+  Serial.begin(9600); 
+}
+
+void loop() {
+  receivedData = 0;  
+
+ 
+  while (digitalRead(pinData) == LOW) {
+    
+  }
+
+
+  delay(50); 
+
+ 
+  for (int i = 7; i >= 0; i--) {
+    bool bit = digitalRead(pinReceive);  
+    Serial.print("Lecture du bit: ");
+    Serial.println(bit);  
+
+    if (bit) {
+      receivedData |= (1 << i);  
+    }
+    delay(100);  
+  }
+
+  
+  Serial.print("Octet reçu: ");
+  Serial.println(receivedData, DEC);
+
+  delay(1000);  
+}
+
